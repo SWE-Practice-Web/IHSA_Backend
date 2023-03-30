0de2eaa9f01b7a7efbdbdfbbca47abf3a3a3cd63
outwrest <0utWrest@protonmail.com>
2023-03-30 14:03:28 -0500

    Init login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IHSA_Backend.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController
+    {
+        
+    }
+}
