41bfbac8d94a7bdf2ad498ec3a2f121b7ee0cd54
Salah Abbas <OutWrest@protonmail.com>
2023-04-06 14:06:34 -0500

    Updated property and event model

--- Models/EventModel.cs
+++ Models/EventModel.cs
@@ -10,14 +10,14 @@ namespace IHSA_Backend.Models
         [FirestoreProperty] public int Id { get; set; }
         [FirestoreProperty] public string? Location { get; set; }
         [FirestoreProperty] public DateTime EventTime { get; set; }
-        [FirestoreProperty] public IEnumerable<int>? Riders { get; set; }
+        [FirestoreProperty] public IEnumerable<EventElementOrderModel> EventOrder { get; set; } = new List<EventElementOrderModel>();
         [FirestoreProperty] public int Zone { get; set; }
     }
     public class EventRequestModel
     {
         public string? Location { get; set; }
         public DateTime EventTime { get; set; }
-        public IEnumerable<int>? Riders { get; set; }
+        public IEnumerable<EventElementOrderRequestModel> EventOrder { get; set; } = new List<EventElementOrderRequestModel>();
         public int Zone { get; set; }
     }
     public class EventResponseModel : EventRequestModel
