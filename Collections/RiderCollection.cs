618c95bec50801bf4f9a4b17709bc80b4d62dac7
outwrest <0utWrest@protonmail.com>
2023-04-24 09:41:06 -0500

    Update rider collection to avert recursive call

--- Collections/RiderCollection.cs
+++ Collections/RiderCollection.cs
@@ -22,7 +22,7 @@ namespace IHSA_Backend.Collections
             _baseCollection = new BaseCollection<RiderModel>(_collectionRef);
 
             _riderCache = new Dictionary<int, RiderModel>();
-            foreach (var rider in GetAllAsync().Result)
+            foreach (var rider in _baseCollection.GetAllAsync().Result)
             {
                 _riderCache.Add(rider.RiderId, rider);
             }
@@ -33,7 +33,7 @@ namespace IHSA_Backend.Collections
 
             var riders = await _baseCollection.GetAllAsync();
             
-            foreach (var rider in GetAllAsync().Result)
+            foreach (var rider in riders)
             {
                 _riderCache.Add(rider.RiderId, rider);
             }
